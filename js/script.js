88e009aef4dbfafc3ef3e8217d13b0ef8976c134
nahid <nahidraj.web@gmail.com>
2023-02-21 14:16:49 +0600

    update index.html and add js file

---
+++ js/script.js
@@ -0,0 +1,60 @@
+// operator
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(10 % 3);
+// console.log(10 ** 3);
+
+// let a = 10;
+
+// console.log(++a);
+
+// console.log(10 * 2 / 5);
+
+// console.log((12*10) / 5);
+
+// let b = 20;
+// console.log('10' == 10);
+// console.log('10' === 10);
+
+// console.log("Hello" + " World");
+// console.log('1' + 1);
+
+// console.log(typeof Number("10"));
+
+// let firstName;
+
+// console.log(firstName);
+
+// console.log(undefined == null);
+
+// primitive data type
+// console.log(typeof "fesaf");
+// console.log(typeof 45);
+// console.log(typeof undefined);
+// console.log(undefined == null);
+// console.log(typeof Symbol());
+// console.log(typeof true);
+// console.log(typeof false)
+
+// // object data type
+// console.log(typeof {})
+// console.log(typeof function () {  })
+// console.log(Array.isArray([]))
+// console.log(typeof 55)
+
+// let random = "fsfeasfasef"
+
+// console.log(Number.isNaN(Number(random)));
+
+// const obj = {
+//   a: 1,
+//   b: 2,
+//   "a b": "hi",
+// };
+
+// console.log(obj["b"]);
+
+// console.log(obj["a b"]);
